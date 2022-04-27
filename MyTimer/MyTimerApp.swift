4500ded81790c23d37edc3818ce1414d8cf1431f
kojima rikuto <kojikoji7765@icloud.com>
2022-04-27 21:04:20 +0900

    Initial Commit

---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by 児島　陸斗 on 2022/04/27.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
