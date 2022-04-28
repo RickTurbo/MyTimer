598b1b07827d215f6e484285683534afebc729eb
kojima rikuto <kojikoji7765@icloud.com>
2022-04-28 16:54:16 +0900

    アラートの追加

--- MyTimer/ContentView.swift
+++ MyTimer/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @State var timerHandler : Timer?
     @State var count = 0
     @AppStorage("timer_value") var timerValue = 10
+    @State var showAlert = false
 
     var body: some View {
         NavigationView {
@@ -65,6 +66,11 @@ struct ContentView: View {
                     }
                 }
             }
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("終了"),
+                message: Text("タイマー終了時間です"),
+                      dismissButton: .default(Text("OK")))
+            }
         }
         .navigationViewStyle(StackNavigationViewStyle())
     }
@@ -73,6 +79,7 @@ struct ContentView: View {
         count += 1
         if timerValue - count <= 0 {
             timerHandler?.invalidate()
+            showAlert = true
         }
     }
 
